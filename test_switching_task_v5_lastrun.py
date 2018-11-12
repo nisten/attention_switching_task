43c861ca2fcea5b12b63e0e98ae2ea64fe6534e6
nisten <nisten@outlook.com>
2018-11-12 12:27:47 -0500

    color

--- test_switching_task_v5_lastrun.py
+++ test_switching_task_v5_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.0.0b10),
-    on Mon Nov 12 12:21:57 2018
+    on Mon Nov 12 12:26:56 2018
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
